f43ea6ea6bcab8fbe5db05691f943ae1aeca5742
nathluu <luuhoangtanbis@gmail.com>
2023-04-19 21:41:49 +0700

    add provider

---
+++ provider.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+    subscription_id = var.usr-subscription-id
+    tenant_id = var.usr-tenant-id
+    client_id = var.usr-client-id
+    client_secret = var.usr-client-secret
+    skip_provider_registration = var.usr-skip-provider-registration
+    features {}
+}
+
+terraform {
+    require_version = ">= 0.13"
+    require_provider {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "2.98"
+        }
+    }
+}
